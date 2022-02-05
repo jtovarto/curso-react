703a34326498a8ff02098007bd2fa15edb5db141
Johan Tovar <jtovarto@gmail.com>
2022-02-05 14:35:05 -0400

    Add amount validation in question component

--- budget-manager/src/components/Question.jsx
+++ budget-manager/src/components/Question.jsx
@@ -1,12 +1,30 @@
+import { useState } from "react";
+
 const Question = () => {
+  const [amount, updateAmount] = useState(0);
+  const [errors, updateErrors] = useState(false);
+
+  const addBudget = (e) => {
+    e.preventDefault();
+
+    if (amount < 1 || isNaN(amount)) {
+      updateErrors(true);
+      return;
+    }
+    updateErrors(false);
+    
+  };
+
   return (
     <>
       <h2>Coloca tu presupuesto</h2>
-      <form>
+      <form onSubmit={addBudget}>
         <input
           type="number"
           className="u-full-width"
           placeholder="Coloca tu presupuesto"
+          onChange={(e) => updateAmount(parseInt(e.target.value, 10))}
+          value={amount}
         />
 
         <input
