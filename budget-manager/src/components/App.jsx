a7611a8895a98c246d4336382a5d1c1a5e7dd5db
Johan Tovar <jtovarto@gmail.com>
2022-02-05 12:53:11 -0400

    Initial commit

---
+++ budget-manager/src/components/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Gasto Semanal</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
