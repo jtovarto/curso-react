4b4720d1b1dc69822df3d97e64b58ea337d45c33
Johan Tovar <jtovarto@gmail.com>
2022-02-10 13:05:13 -0400

    Add SwitchTransition example

--- transition-examples/src/components/Transition03.jsx
+++ transition-examples/src/components/Transition03.jsx
@@ -1,5 +1,47 @@
+import { useState } from "react";
+import { SwitchTransition, CSSTransition } from "react-transition-group";
+import "./Transition03.css";
+import Form from "./Form";
+import Welcome from "./Welcome";
+
 const Transition03 = () => {
-    return ( <p>Transition 03</p>  );
-}
- 
+  const [state, setState] = useState(false);
+  const [username, setUsername] = useState("");
+
+  const transitionendEvent = (node, done) => {
+    node.addEventListener("transitionend", done, false);
+  };
+  const switchKey = () => {
+    return state ? "form" : "welcome";
+  };
+
+  return (
+    <>
+      <SwitchTransition mode={"out-in"}>
+        <CSSTransition
+          key={state ? "form" : "welcome"}
+          addEndListener={transitionendEvent}
+          classNames="fade"
+        >
+          <div style={{ height: "300px" }}>
+            {state ? (
+              <Welcome username={username} />
+            ) : (
+              <Form login={setUsername} />
+            )}
+          </div>
+        </CSSTransition>
+      </SwitchTransition>
+      <div class="text-center mt-4">
+        <button
+          className="btn btn-primary shadow"
+          onClick={() => setState((state) => !state)}
+        >
+          {state ? "Login in" : "Close session"}
+        </button>
+      </div>
+    </>
+  );
+};
+
 export default Transition03;
