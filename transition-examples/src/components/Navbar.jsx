8510a8a1b83d9456101d4858374b390a77f128ef
Johan Tovar <jtovarto@gmail.com>
2022-02-10 11:34:36 -0400

    fix: remove classNameActive form navlinks

--- transition-examples/src/components/Navbar.jsx
+++ transition-examples/src/components/Navbar.jsx
@@ -4,34 +4,22 @@ const Navbar = () => {
   return (
     <ul className="nav nav-pills nav-fill">
       <li className="nav-item">
-        <NavLink to="/" className="nav-link" activeClassName="active">
+        <NavLink to="/" className="nav-link">
           Transition
         </NavLink>
       </li>
       <li className="nav-item">
-        <NavLink
-          to="/ccs-transition"
-          className="nav-link"
-          activeClassName="active"
-        >
+        <NavLink to="/ccs-transition" className="nav-link">
           CSSTransition
         </NavLink>
       </li>
       <li className="nav-item">
-        <NavLink
-          to="/switch-transition"
-          className="nav-link"
-          activeClassName="active"
-        >
+        <NavLink to="/switch-transition" className="nav-link">
           SwitchTransition
         </NavLink>
       </li>
       <li className="nav-item">
-        <NavLink
-          to="/transition-group"
-          className="nav-link"
-          activeClassName="active"
-        >
+        <NavLink to="/transition-group" className="nav-link">
           TransitionGroup
         </NavLink>
       </li>
