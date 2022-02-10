5a561635bbcef246c62e2323ea6cfe316cf21ee0
Johan Tovar <jtovarto@gmail.com>
2022-02-10 11:22:27 -0400

    Add react routes

--- transition-examples/src/App.jsx
+++ transition-examples/src/App.jsx
@@ -1,21 +1,28 @@
-import { useState } from 'react'
-import Navbar from './components/Navbar'
-import Transition01 from './components/Transition01';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Transition01 from "./components/Transition01";
+import Transition02 from "./components/Transition02";
+import Transition03 from "./components/Transition03";
+import Transition04 from "./components/Transition04";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="container">
-      <header className="p-4 border-bottom">
-        <Navbar />
-      </header>
-
-      <div class="mt-4">
-        <Transition01/>
-      </div>
+      <BrowserRouter>
+        <header className="p-4 border-bottom">
+          <Navbar />
+        </header>
+        <div className="mt-4">
+          <Routes>
+            <Route path="/" element={<Transition01 />} />
+            <Route path="/ccs-transition" element={<Transition02 />} />
+            <Route path="/switch-transition" element={<Transition03 />} />
+            <Route path="/transition-group" element={<Transition04 />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App
+export default App;
--- transition-examples/src/components/Navbar.jsx
+++ transition-examples/src/components/Navbar.jsx
@@ -1,23 +1,39 @@
+import { NavLink } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <ul className="nav nav-pills nav-fill">
       <li className="nav-item">
-        <a className="nav-link active" aria-current="page" href="#">
-          Active
-        </a>
+        <NavLink to="/" className="nav-link" activeClassName="active">
+          Transition
+        </NavLink>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">
-          Much longer nav link
-        </a>
+        <NavLink
+          to="/ccs-transition"
+          className="nav-link"
+          activeClassName="active"
+        >
+          CSSTransition
+        </NavLink>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">
-          Link
-        </a>
+        <NavLink
+          to="/switch-transition"
+          className="nav-link"
+          activeClassName="active"
+        >
+          SwitchTransition
+        </NavLink>
       </li>
       <li className="nav-item">
-        <a className="nav-link disabled">Disabled</a>
+        <NavLink
+          to="/transition-group"
+          className="nav-link"
+          activeClassName="active"
+        >
+          TransitionGroup
+        </NavLink>
       </li>
     </ul>
   );
