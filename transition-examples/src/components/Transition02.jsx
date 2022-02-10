507b64522affeb939daea37b7da53dd84ac4fb76
Johan Tovar <jtovarto@gmail.com>
2022-02-10 12:25:32 -0400

    Add transition 2 example

--- transition-examples/src/components/Transition02.jsx
+++ transition-examples/src/components/Transition02.jsx
@@ -1,5 +1,64 @@
+import { useState } from "react";
+import { CSSTransition } from "react-transition-group";
+import "./Transition02.css";
+
 const Transition02 = () => {
-    return ( <p>Transition 02</p>  );
-}
- 
+  const [showMessage, setShowMessage] = useState(false);
+  return (
+    <div className="text-center">
+      <button
+        className="btn btn-primary mb-4"
+        onClick={() => setShowMessage(!showMessage)}
+      >
+        Show Message
+      </button>
+      <CSSTransition
+        in={showMessage}
+        timeout={1000}
+        classNames="example"
+        unmountOnExit
+      >
+        <div
+          className="alert alert-success d-flex align-items-center"
+          role="alert"
+        >
+          <svg
+            className="bi flex-shrink-0 me-2"
+            width="24"
+            height="24"
+            role="img"
+            aria-label="Success:"
+          >
+            <use xlinkHref="#check-circle-fill" />
+          </svg>
+          <div>Exits to the left</div>
+        </div>
+      </CSSTransition>
+
+      <CSSTransition
+        in={showMessage}
+        timeout={1000}
+        classNames="example2"
+        unmountOnExit
+      >
+        <div
+          className="alert alert-info d-flex align-items-center w-50 translate"
+          role="alert"
+        >
+          <svg
+            className="bi flex-shrink-0 me-2"
+            width="24"
+            height="24"
+            role="img"
+            aria-label="Success:"
+          >
+            <use xlinkHref="#check-circle-fill" />
+          </svg>
+          <div>Exits to the right</div>
+        </div>
+      </CSSTransition>
+    </div>
+  );
+};
+
 export default Transition02;
