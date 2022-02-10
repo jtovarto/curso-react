f396348e41b375376bd79e409836117619f6c1cf
Johan Tovar <jtovarto@gmail.com>
2022-02-10 11:34:49 -0400

    Add transition 01 example

--- transition-examples/src/components/Transition01.jsx
+++ transition-examples/src/components/Transition01.jsx
@@ -1,5 +1,57 @@
+import { useState } from "react";
+import { Transition } from "react-transition-group";
+
 const Transition01 = () => {
-    return ( <p>Transition 01</p>  );
-}
- 
+  const duration = 1000;
+
+  const defaultStyle = {
+    transition: `opacity ${duration}ms ease-in-out`,
+    opacity: 0,
+  };
+
+  const transitionStyles = {
+    entering: { opacity: 0 },
+    entered: { opacity: 1 },
+    exiting: { opacity: 1 },
+    exited: { opacity: 0 },
+  };
+
+  const [inProp, setInProp] = useState(false);
+
+  return (
+    <div className="text-center">
+      <button
+        className="btn btn-primary mb-4"
+        onClick={() => setInProp(!inProp)}
+      >
+        Click to Show
+      </button>
+      <Transition in={inProp} timeout={300}>
+        {(state) => (
+          <div
+            className="alert alert-success"
+            role="alert"
+            style={{
+              ...defaultStyle,
+              ...transitionStyles[state],
+            }}
+          >
+            <h4 className="alert-heading">Well done!</h4>
+            <p>
+              Aww yeah, you successfully read this important alert message. This
+              example text is going to run a bit longer so that you can see how
+              spacing within an alert works with this kind of content.
+            </p>
+            <hr />
+            <p className="mb-0">
+              Whenever you need to, be sure to use margin utilities to keep
+              things nice and tidy.
+            </p>
+          </div>
+        )}
+      </Transition>
+    </div>
+  );
+};
+
 export default Transition01;
