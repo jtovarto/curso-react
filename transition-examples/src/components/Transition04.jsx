eb0ece3a72a968b1c708f2a25c9c8f1bf03612a3
Johan Tovar <jtovarto@gmail.com>
2022-02-11 16:50:13 -0400

    Add transition 04 example

--- transition-examples/src/components/Transition04.jsx
+++ transition-examples/src/components/Transition04.jsx
@@ -1,5 +1,61 @@
+import { useState } from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import "./Transitions.css";
+
 const Transition04 = () => {
-    return ( <p>Transition 04</p>  );
-}
- 
+  const [items, setItems] = useState(["🍏 Apple", "🍌 Banana"]);
+
+  const FRUITS = [
+    "🍏 Apple",
+    "🍌 Banana",
+    "🍍 Pineapple",
+    "🥥 Coconut",
+    "🍉 Watermelon",
+    "🍓 Strawberry",
+    "🍈 Melon",
+    "🥝 Kiwi",
+    "🍒 Cherry",
+    "🍑 Peach",
+  ];
+
+  const handleAddFruit = () => {
+    const nextHiddenItem = FRUITS.find((i) => !items.includes(i));
+    if (nextHiddenItem) {
+      setItems((prev) => [...prev, nextHiddenItem]);
+    }
+  };
+
+  const transitionendEvent = (node, done) => {
+    node.addEventListener("transitionend", done, false);
+  };
+  return (
+    <div>
+      <TransitionGroup>
+        <h2 className="text-center">Shopping List</h2>
+        <div class="list-group w-25 mx-auto btn-group">
+          {items.map((item, index) => (
+            <CSSTransition
+              key={index}
+              timeout={500}
+              classNames="example"
+              addEndListener={transitionendEvent}
+            >
+              <a
+                href="#"
+                className="list-group-item list-group-item-action"
+                aria-current="true"
+              >
+                {item}
+              </a>
+            </CSSTransition>
+          ))}
+          <button class="btn btn-dark rounded-0" onClick={handleAddFruit}>
+            Add Fruit
+          </button>
+        </div>
+      </TransitionGroup>
+    </div>
+  );
+};
+
 export default Transition04;
